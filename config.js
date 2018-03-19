4f85d6ad72c1b7c8f60242a89238962d5c30c8bb
Louis Mazza <louismazza@gmail.com>
2018-03-18 21:00:16 -0700

    attempting mlab connection

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 'use strict';
 
-exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/MoW-Capstone';
+exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://lemazza:12r46a@ds117509.mlab.com:17509/huntersoftheweek' ||'mongodb://localhost/MoW-Capstone';
 exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost/Test-MoW-Capstone';
 
 
